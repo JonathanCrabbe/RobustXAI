Fix model invariance computation redundancy

experiments/ecg.py
@@ -11,21 +11,12 @@ from datasets.loaders import ECGDataset
 from models.time_series import AllCNN, StandardCNN
 from utils.symmetries import Translation1D
 from utils.misc import set_random_seed
-from utils.plots import robustness_plots, relaxing_invariance_plots, understanding_randomness_plots
-from itertools import product
+from utils.plots import robustness_plots, relaxing_invariance_plots
 from interpretability.robustness import model_invariance, explanation_equivariance, explanation_invariance
 from interpretability.example import SimplEx, RepresentationSimilarity, TracIN, InfluenceFunctions
 from interpretability.feature import FeatureImportance
 from captum.attr import IntegratedGradients, GradientShap, FeaturePermutation, FeatureAblation, Occlusion
 from sklearn.metrics import mean_absolute_error
-
-
-concept_to_class = {
-    "Supraventricular": 1,
-    "Premature Ventricular": 2,
-    "Fusion Beats": 3,
-    "Unknown": 4,
-}
 
 
 def train_ecg_model(
@@ -65,44 +56,42 @@ def feature_importance(
     model_name: str = "model",
     model_dir: Path = Path.cwd() / f"results/ecg/",
     data_dir: Path = Path.cwd() / "datasets/ecg",
+    n_test: int = 1000
 ) -> None:
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     set_random_seed(random_seed)
     test_set = ECGDataset(data_dir, train=False, balance_dataset=False)
-    test_loader = DataLoader(test_set, batch_size, shuffle=True)
+    test_subset = Subset(test_set, torch.randperm(len(test_set))[:n_test])
+    test_loader = DataLoader(test_subset, batch_size)
     models = {'All-CNN': AllCNN(latent_dim, f'{model_name}_allcnn'),
               'Standard-CNN': StandardCNN(latent_dim, f'{model_name}_standard'),
-              'Augmented-CNN': StandardCNN(latent_dim, f'{model_name}_augmented')
-              }
-    attr_methods = {'Integrated Gradients': IntegratedGradients,
-                    'Gradient Shap': GradientShap,
-                    'Feature Permutation': FeaturePermutation,
-                    'Feature Ablation': FeatureAblation,
-                    'Feature Occlusion': Occlusion
-                    }
+              'Augmented-CNN': StandardCNN(latent_dim, f'{model_name}_augmented')}
+    attr_methods = {'Integrated Gradients': IntegratedGradients, 'Gradient Shap': GradientShap,
+                    'Feature Permutation': FeaturePermutation,'Feature Ablation': FeatureAblation,
+                    'Feature Occlusion': Occlusion}
     model_dir = model_dir/model_name
     save_dir = model_dir/'feature_importance'
     if not save_dir.exists():
         os.makedirs(save_dir)
     translation = Translation1D()
     metrics = []
-    for model_type, attr_name in product(models, attr_methods):
-        logging.info(f'Now working with classifier = {model_type} and explainer = {attr_name}')
+    for model_type in models:
+        logging.info(f'Now working with {model_type} classifier')
         model = models[model_type]
-        if model_type != 'Random-CNN':
-            model.load_state_dict(torch.load(model_dir/f"{model.name}.pt"), strict=False)
-        model.to(device)
-        model.eval()
-        feat_importance = FeatureImportance(attr_methods[attr_name](model))
-        model_inv = model_invariance(model, translation, test_loader, device, N_samp=1)
-        explanation_equiv = explanation_equivariance(feat_importance, translation, test_loader, device, N_samp=1)
-        for inv, equiv in zip(model_inv, explanation_equiv):
-            metrics.append([model_type, attr_name, inv.item(), equiv.item()])
+        model.load_metadata(model_dir)
+        model.load_state_dict(torch.load(model_dir / f"{model.name}.pt"), strict=False)
+        model.to(device).eval()
+        model_inv = model_invariance(model, translation, test_loader, device, N_samp=50)
         logging.info(f'Model invariance: {torch.mean(model_inv):.3g}')
-        logging.info(f'Explanation equivariance: {torch.mean(explanation_equiv):.3g}')
-    metrics_df = pd.DataFrame(data=metrics,
-                              columns=['Model Type', 'Explanation', 'Model Invariance', 'Explanation Equivariance'])
+        for attr_name in attr_methods:
+            logging.info(f'Now working with {attr_name} explainer')
+            feat_importance = FeatureImportance(attr_methods[attr_name](model))
+            explanation_equiv = explanation_equivariance(feat_importance, translation, test_loader, device, N_samp=1)
+            for inv, equiv in zip(model_inv, explanation_equiv):
+                metrics.append([model_type, attr_name, inv.item(), equiv.item()])
+            logging.info(f'Explanation equivariance: {torch.mean(explanation_equiv):.3g}')
+    metrics_df = pd.DataFrame(data=metrics, columns=['Model Type', 'Explanation', 'Model Invariance', 'Explanation Equivariance'])
     metrics_df.to_csv(save_dir/'metrics.csv', index=False)
     if plot:
         robustness_plots(save_dir, 'ecg', 'feature_importance')
@@ -117,63 +106,50 @@ def example_importance(
     model_name: str = "model",
     model_dir: Path = Path.cwd() / f"results/ecg/",
     data_dir: Path = Path.cwd() / "datasets/ecg",
-    n_test: int = 1000
+    n_test: int = 1000,
+    n_train: int = 100,
+    recursion_depth: int = 100
 ) -> None:
-
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     set_random_seed(random_seed)
     train_set = ECGDataset(data_dir, train=False, balance_dataset=False)
-    train_loader = DataLoader(train_set, 100, shuffle=True)
+    train_loader = DataLoader(train_set, n_train, shuffle=True)
     X_train, Y_train = next(iter(train_loader))
-    X_train = X_train.to(device)
-    Y_train = Y_train.to(device)
+    X_train, Y_train = X_train.to(device), Y_train.to(device)
+    train_sampler = RandomSampler(train_set, replacement=True, num_samples=recursion_depth*batch_size)
+    train_loader_replacement = DataLoader(train_set, batch_size, sampler=train_sampler)
     test_set = ECGDataset(data_dir, train=False, balance_dataset=False)
-    test_subset = Subset(test_set, torch.randint(low=0, high=len(test_set)-1, size=[n_test]))
+    test_subset = Subset(test_set, torch.randperm(len(test_set))[:n_test])
     test_loader = DataLoader(test_subset, batch_size)
     models = {'All-CNN': AllCNN(latent_dim, f'{model_name}_allcnn'),
               'Standard-CNN': StandardCNN(latent_dim, f'{model_name}_standard'),
-              'Augmented-CNN': StandardCNN(latent_dim, f'{model_name}_augmented'),
-              }
-    attr_methods = {'Influence Functions': InfluenceFunctions,
-                    'TracIn': TracIN,
-                    'SimplEx': SimplEx,
-                    'Representation Similarity': RepresentationSimilarity,
-                    }
+              'Augmented-CNN': StandardCNN(latent_dim, f'{model_name}_augmented')}
+    attr_methods = {'Influence Functions': InfluenceFunctions, 'TracIn': TracIN, 'SimplEx': SimplEx,
+                    'Representation Similarity': RepresentationSimilarity}
     model_dir = model_dir/model_name
     save_dir = model_dir/'example_importance'
     if not save_dir.exists():
         os.makedirs(save_dir)
     translation = Translation1D()
     metrics = []
-    for model_type, attr_name in product(models, attr_methods):
-        logging.info(f'Now working with classifier = {model_type} and explainer = {attr_name}')
+    for model_type in models:
+        logging.info(f'Now working with {model_type} classifier')
         model = models[model_type]
-        if model_type != 'Random-CNN':
-            model.load_metadata(model_dir)
-            model.load_state_dict(torch.load(model_dir/f"{model.name}.pt"), strict=False)
-        model.to(device)
-        model.eval()
-        match attr_name:
-            case 'Influence Functions':
-                recursion_depth = 100
-                train_sampler = RandomSampler(train_set, replacement=True, num_samples=recursion_depth * batch_size)
-                train_loader_replacement = DataLoader(train_set, batch_size, sampler=train_sampler)
-                ex_importance = attr_methods[attr_name](model, X_train, Y_train, train_loader_replacement,
-                                                        nn.CrossEntropyLoss(), batch_size,
-                                                        save_dir/'influence_functions'/model.name)
-            case 'TracIn':
-                ex_importance = attr_methods[attr_name](model, X_train, Y_train,
-                                                        nn.CrossEntropyLoss(reduction='none'), batch_size=batch_size)
-            case other:
-                ex_importance = attr_methods[attr_name](model, X_train)
+        model.load_metadata(model_dir)
+        model.load_state_dict(torch.load(model_dir / f"{model.name}.pt"), strict=False)
+        model.to(device).eval()
         model_inv = model_invariance(model, translation, test_loader, device, N_samp=50)
-        explanation_inv = explanation_invariance(ex_importance, translation, test_loader, device, N_samp=1)
-        for inv_model, inv_expl in zip(model_inv, explanation_inv):
-            metrics.append([model_type, attr_name, inv_model.item(), inv_expl.item()])
         logging.info(f'Model invariance: {torch.mean(model_inv):.3g}')
-        logging.info(f'Explanation invariance: {torch.mean(explanation_inv):.3g}')
-    metrics_df = pd.DataFrame(data=metrics,
-                              columns=['Model Type', 'Explanation', 'Model Invariance', 'Explanation Invariance'])
+        for attr_name in attr_methods:
+            logging.info(f'Now working with {attr_name} explainer')
+            ex_importance = attr_methods[attr_name](model, X_train, Y_train=Y_train, train_loader=train_loader_replacement,
+                                                    loss_function=nn.CrossEntropyLoss(), batch_size=batch_size,
+                                                    save_dir=save_dir /'influence_functions'/model.name, recursion_depth=recursion_depth)
+            explanation_inv = explanation_invariance(ex_importance, translation, test_loader, device, N_samp=1)
+            for inv_model, inv_expl in zip(model_inv, explanation_inv):
+                metrics.append([model_type, attr_name, inv_model.item(), inv_expl.item()])
+            logging.info(f'Explanation invariance: {torch.mean(explanation_inv):.3g}')
+    metrics_df = pd.DataFrame(data=metrics, columns=['Model Type', 'Explanation', 'Model Invariance', 'Explanation Invariance'])
     metrics_df.to_csv(save_dir/'metrics.csv', index=False)
     if plot:
         robustness_plots(save_dir, 'ecg', 'example_importance')
@@ -228,15 +204,16 @@ if __name__ == "__main__":
     parser.add_argument("--latent_dim", type=int, default=32)
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--plot", action="store_true")
+    parser.add_argument("--n_test", type=int, default=1000)
     args = parser.parse_args()
     model_name = f"cnn{args.latent_dim}_seed{args.seed}"
     if args.train:
         train_ecg_model(args.seed, args.latent_dim, args.batch_size, model_name=model_name)
     match args.name:
         case 'feature_importance':
-            feature_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name)
+            feature_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name, n_test=args.n_test)
         case 'example_importance':
-            example_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name)
+            example_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name, n_test=args.n_test)
         case 'understand_randomness':
             understand_randomness(args.seed, args.latent_dim, args.batch_size, args.plot, model_name)
         case other:

interpretability/example.py
@@ -20,7 +20,7 @@ class ExampleBasedExplainer(nn.Module, ABC):
 
 
 class SimplEx(ExampleBasedExplainer):
-    def __init__(self, model: nn.Module,  X_train: torch.Tensor):
+    def __init__(self, model: nn.Module,  X_train: torch.Tensor, **kwargs):
         super().__init__(model, X_train)
         self.H_train = self.model.representation(self.X_train).detach().flatten(start_dim=1)
 
@@ -48,19 +48,19 @@ class SimplEx(ExampleBasedExplainer):
 
 
 class RepresentationSimilarity(ExampleBasedExplainer):
-    def __init__(self, model: nn.Module,  X_train: torch.Tensor):
+    def __init__(self, model: nn.Module,  X_train: torch.Tensor, **kwargs):
         super().__init__(model, X_train)
         self.H_train = self.model.representation(self.X_train).detach().flatten(start_dim=1).unsqueeze(0)
 
     def forward(self, x, y) -> torch.Tensor:
-        h_test = self.model.representation(x).detach().flatten(start_dim=1).unsqueeze(1)
-        attribution = F.cosine_similarity(self.H_train, h_test, dim=-1)
+        H = self.model.representation(x).detach().flatten(start_dim=1).unsqueeze(1)
+        attribution = F.cosine_similarity(self.H_train, H, dim=-1)
         return attribution
 
 
 class TracIN(ExampleBasedExplainer):
     def __init__(self, model: nn.Module,  X_train: torch.Tensor,  Y_train: torch.Tensor,
-                 loss_function: callable, batch_size: int = 1):
+                 loss_function: callable, batch_size: int = 1, **kwargs):
         super().__init__(model, X_train)
         train_subset = TensorDataset(X_train, Y_train)
         last_layer = model.last_layer()
@@ -78,7 +78,7 @@ class TracIN(ExampleBasedExplainer):
 
 class InfluenceFunctions(ExampleBasedExplainer):
     def __init__(self, model: nn.Module,  X_train: torch.Tensor,  Y_train: torch.Tensor, train_loader: DataLoader,
-                 loss_function: callable, batch_size: int, save_dir: Path):
+                 loss_function: callable, batch_size: int, save_dir: Path, recursion_depth: int,  **kwargs):
         super().__init__(model, X_train)
         self.last_layer = model.last_layer()
         self.train_loader = train_loader
@@ -90,17 +90,18 @@ class InfluenceFunctions(ExampleBasedExplainer):
         self.subtrain_loader = DataLoader(train_subset, batch_size=1, shuffle=False)
         self.device = self.X_train.device
         self.save_dir = save_dir
+        self.recursion_depth = recursion_depth
         if not save_dir.exists():
             os.makedirs(save_dir)
 
-    def evaluate_ihvp(self, recursion_depth: int = 100,  damp: float = 1e-3, scale: float = 1000,) -> None:
+    def evaluate_ihvp(self,  damp: float = 1e-3, scale: float = 1000,) -> None:
         for train_idx, (x_train, y_train) in enumerate(self.subtrain_loader):
             x_train = x_train.to(self.device)
             loss = self.loss_function(self.model(x_train), y_train)
             grad = self.direct_sum(torch.autograd.grad(loss, self.last_layer.parameters(), create_graph=True))
             ihvp = grad.detach().clone()
             train_sampler = iter(self.train_loader)
-            for _ in range(recursion_depth):
+            for _ in range(self.recursion_depth):
                 X_sample, Y_sample = next(train_sampler)
                 X_sample, Y_sample = X_sample.to(self.device), Y_sample.to(self.device)
                 sampled_loss = self.loss_function(self.model(X_sample), Y_sample)