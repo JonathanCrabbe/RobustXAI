Implement concept explainers, improve ECG All-CNN, fix scaling bug in Influence Functions

datasets/loaders.py
@@ -6,15 +6,22 @@ import numpy as np
 from pathlib import Path
 from torch.utils.data import Dataset, SubsetRandomSampler
 from imblearn.over_sampling import SMOTE
+from abc import ABC, abstractmethod, abstractproperty
 
 
-class ECGDataset(Dataset):
-    concept_to_class = {
-        "Supraventricular": 1,
-        "Premature Ventricular": 2,
-        "Fusion Beats": 3,
-        "Unknown": 4,
-    }
+class ConceptDataset(ABC, Dataset):
+
+    @property
+    @abstractmethod
+    def concept_names(self):
+        ...
+
+    @abstractmethod
+    def generate_concept_dataset(self, concept_id: int, concept_set_size: int) -> tuple:
+        ...
+
+
+class ECGDataset(ConceptDataset):
 
     def __init__(self, data_dir: Path, train: bool, balance_dataset: bool,
                  random_seed: int = 42, binarize_label: bool = True):
@@ -71,14 +78,17 @@ class ECGDataset(Dataset):
             a concept dataset of the form X (features),C (concept labels)
         """
         assert not self.binarize_label
-        mask = self.y == concept_id
+        mask = self.y == concept_id + 1
         positive_idx = torch.nonzero(mask).flatten()
         negative_idx = torch.nonzero(~mask).flatten()
         positive_loader = torch.utils.data.DataLoader(self, batch_size=concept_set_size, sampler=SubsetRandomSampler(positive_idx))
         negative_loader = torch.utils.data.DataLoader(self, batch_size=concept_set_size, sampler=SubsetRandomSampler(negative_idx))
         X_pos, C_pos = next(iter(positive_loader))
         X_neg, C_neg = next(iter(negative_loader))
-        X = np.concatenate((X_pos.cpu().numpy(), X_neg.cpu().numpy()), 0)
-        C = np.concatenate((np.ones(concept_set_size), np.zeros(concept_set_size)), 0)
-        rand_perm = np.random.permutation(len(X))
+        X = torch.concatenate((X_pos, X_neg), 0)
+        C = torch.concatenate((torch.ones(concept_set_size), torch.zeros(concept_set_size)), 0)
+        rand_perm = torch.randperm(len(X))
         return X[rand_perm], C[rand_perm]
+
+    def concept_names(self):
+        return ["Supraventricular", "Premature Ventricular", "Fusion Beats", "Unknown"]

experiments/ecg.py
@@ -12,9 +12,10 @@ from models.time_series import AllCNN, StandardCNN
 from utils.symmetries import Translation1D
 from utils.misc import set_random_seed
 from utils.plots import robustness_plots, relaxing_invariance_plots
-from interpretability.robustness import model_invariance, explanation_equivariance, explanation_invariance
+from interpretability.robustness import model_invariance, explanation_equivariance, explanation_invariance, accuracy
 from interpretability.example import SimplEx, RepresentationSimilarity, TracIN, InfluenceFunctions
 from interpretability.feature import FeatureImportance
+from interpretability.concept import CAR, CAV
 from captum.attr import IntegratedGradients, GradientShap, FeaturePermutation, FeatureAblation, Occlusion
 from sklearn.metrics import mean_absolute_error
 
@@ -112,7 +113,7 @@ def example_importance(
 ) -> None:
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     set_random_seed(random_seed)
-    train_set = ECGDataset(data_dir, train=False, balance_dataset=False)
+    train_set = ECGDataset(data_dir, train=True, balance_dataset=False)
     train_loader = DataLoader(train_set, n_train, shuffle=True)
     X_train, Y_train = next(iter(train_loader))
     X_train, Y_train = X_train.to(device), Y_train.to(device)
@@ -154,6 +155,57 @@ def example_importance(
     if plot:
         robustness_plots(save_dir, 'ecg', 'example_importance')
         relaxing_invariance_plots(save_dir, 'ecg', 'example_importance')
+
+
+def concept_importance(
+    random_seed: int,
+    latent_dim: int,
+    batch_size: int,
+    plot: bool,
+    model_name: str = "model",
+    model_dir: Path = Path.cwd() / f"results/ecg/",
+    data_dir: Path = Path.cwd() / "datasets/ecg",
+    n_test: int = 1000,
+    concept_set_size: int = 100,
+) -> None:
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    set_random_seed(random_seed)
+    train_set = ECGDataset(data_dir, train=True, binarize_label=False, balance_dataset=False)
+    test_set = ECGDataset(data_dir, train=False, balance_dataset=False)
+    test_subset = Subset(test_set, torch.randperm(len(test_set))[:n_test])
+    test_loader = DataLoader(test_subset, batch_size)
+    models = {'All-CNN': AllCNN(latent_dim, f'{model_name}_allcnn'),
+              'Standard-CNN': StandardCNN(latent_dim, f'{model_name}_standard'),
+              'Augmented-CNN': StandardCNN(latent_dim, f'{model_name}_augmented')}
+    attr_methods = {'CAV': CAV, 'CAR': CAR}
+    model_dir = model_dir/model_name
+    save_dir = model_dir/'concept_importance'
+    if not save_dir.exists():
+        os.makedirs(save_dir)
+    translation = Translation1D()
+    metrics = []
+    for model_type in models:
+        logging.info(f'Now working with {model_type} classifier')
+        model = models[model_type]
+        model.load_metadata(model_dir)
+        model.load_state_dict(torch.load(model_dir / f"{model.name}.pt"), strict=False)
+        model.to(device).eval()
+        model_inv = model_invariance(model, translation, test_loader, device, N_samp=50)
+        logging.info(f'Model invariance: {torch.mean(model_inv):.3g}')
+        for attr_name in attr_methods:
+            logging.info(f'Now working with {attr_name} explainer')
+            conc_importance = attr_methods[attr_name](model, train_set, n_classes=2)
+            conc_importance.fit(device)
+            explanation_inv = explanation_invariance(conc_importance, translation, test_loader, device, N_samp=1,
+                                                     similarity=accuracy)
+            for inv_model, inv_expl in zip(model_inv, explanation_inv):
+                metrics.append([model_type, attr_name, inv_model.item(), inv_expl.item()])
+            logging.info(f'Explanation invariance: {torch.mean(explanation_inv):.3g}')
+    metrics_df = pd.DataFrame(data=metrics, columns=['Model Type', 'Explanation', 'Model Invariance', 'Explanation Invariance'])
+    metrics_df.to_csv(save_dir/'metrics.csv', index=False)
+    if plot:
+        robustness_plots(save_dir, 'ecg', 'concept_importance')
+        relaxing_invariance_plots(save_dir, 'ecg', 'concept_importance')
 
 
 def understand_randomness(
@@ -214,6 +266,8 @@ if __name__ == "__main__":
             feature_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name, n_test=args.n_test)
         case 'example_importance':
             example_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name, n_test=args.n_test)
+        case 'concept_importance':
+            concept_importance(args.seed, args.latent_dim, args.batch_size, args.plot, model_name, n_test=args.n_test)
         case 'understand_randomness':
             understand_randomness(args.seed, args.latent_dim, args.batch_size, args.plot, model_name)
         case other:

interpretability/concept.py
@@ -1,39 +1,34 @@
 import abc
-import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from abc import ABC
 from sklearn.svm import SVC
 from sklearn.linear_model import SGDClassifier
+from datasets.loaders import ConceptDataset
 
 
-class ConceptExplainer(ABC):
+class ConceptExplainer(ABC, nn.Module):
     """
     An abstract class that contains the interface for any post-hoc concept explainer
     """
 
     @abc.abstractmethod
-    def __init__(self, device: torch.device, batch_size: int = 50):
-        self.concept_reps = None
-        self.concept_labels = None
-        self.classifier = None
-        self.device = device
-        self.batch_size = batch_size
+    def __init__(self, model: nn.Module, dataset: ConceptDataset, **kwargs):
+        super(ConceptExplainer, self).__init__()
+        self.model = model
+        self.classifiers = None
+        self.dataset = dataset
 
     @abc.abstractmethod
-    def fit(self, concept_reps: np.ndarray, concept_labels: np.ndarray) -> None:
+    def fit(self, device: torch.device, concept_set_size: int) -> None:
         """
-        Fit the concept classifier to the dataset (latent_reps, concept_labels)
-        Args:
-            concept_reps: latent representations of the examples illustrating the concept
-            concept_labels: labels indicating the presence (1) or absence (0) of the concept
+        Fit a concept classifier for each concept dataset
         """
-        assert concept_reps.shape[0] == concept_labels.shape[0]
-        self.concept_reps = concept_reps
-        self.concept_labels = concept_labels
+        ...
 
     @abc.abstractmethod
-    def predict(self, latent_reps: np.ndarray) -> np.ndarray:
+    def forward(self, x:torch.Tensor, y:torch.Tensor) -> torch.Tensor:
         """
         Predicts the presence or absence of concept importance for the latent representations
         Args:
@@ -41,109 +36,63 @@ class ConceptExplainer(ABC):
         Returns:
             concepts labels indicating the presence (1) or absence (0) of the concept
         """
-
-    def get_concept_reps(self, positive_set: bool) -> np.ndarray:
-        """
-        Get the latent representation of the positive/negative examples
-        Args:
-            positive_set: True returns positive examples, False returns negative examples
-        Returns:
-            Latent representations of the requested set
-        """
-        return self.concept_reps[self.concept_labels == int(positive_set)]
+        ...
 
 
-class CAR(ConceptExplainer, ABC):
-    def __init__(
-        self,
-        device: torch.device,
-        batch_size: int = 100,
-        kernel: str = "rbf",
-        kernel_width: float = None,
-    ):
-        super(CAR, self).__init__(device, batch_size)
+class CAR(ConceptExplainer):
+    def __init__(self, model: nn.Module,  dataset: ConceptDataset, kernel: str = "rbf", **kwargs):
+        super(CAR, self).__init__(model, dataset)
         self.kernel = kernel
-        self.kernel_width = kernel_width
 
-    def fit(self, concept_reps: np.ndarray, concept_labels: np.ndarray) -> None:
-        """
-        Fit the concept classifier to the dataset (latent_reps, concept_labels)
-        Args:
-            concept_reps: latent representations of the examples illustrating the concept
-            concept_labels: labels indicating the presence (1) or absence (0) of the concept
-        """
-        super(CAR, self).fit(concept_reps, concept_labels)
-        classifier = SVC(kernel=self.kernel)
-        classifier.fit(concept_reps, concept_labels)
-        self.classifier = classifier
+    def fit(self, device: torch.device, concept_set_size: int = 100) -> None:
+        classifiers = []
+        for concept_id, concept_name in enumerate(self.dataset.concept_names()):
+            classifier = SVC(kernel=self.kernel)
+            X_train, C_train = self.dataset.generate_concept_dataset(concept_id, concept_set_size)
+            H_train = self.model.representation(X_train.to(device)).flatten(start_dim=1)
+            classifier.fit(H_train.detach().cpu().numpy(), C_train.numpy())
+            classifiers.append(classifier)
+        self.classifiers = classifiers
 
-    def predict(self, latent_reps: np.ndarray) -> np.ndarray:
-        """
-        Predicts the presence or absence of the concept for the latent representations
-        Args:
-            latent_reps: representations of the test examples
-        Returns:
-            concepts labels indicating the presence (1) or absence (0) of the concept
-        """
-        return self.classifier.predict(latent_reps)
+    def forward(self, x: torch.Tensor, y:torch.Tensor) -> torch.Tensor:
+        assert self.classifiers
+        H = (self.model.representation(x)).detach().cpu().flatten(start_dim=1).numpy()
+        C_pred = torch.zeros((len(x), len(self.classifiers)))
+        for concept_id, classifier in enumerate(self.classifiers):
+            #C_pred[:, concept_id] = torch.from_numpy(classifier.decision_function(H))
+            C_pred[:, concept_id] = torch.from_numpy(classifier.predict(H))
+        return C_pred
 
 
-class CAV(ConceptExplainer, ABC):
-    def __init__(self, device: torch.device, batch_size: int = 50):
-        super(CAV, self).__init__(device, batch_size)
+class CAV(ConceptExplainer):
+    def __init__(self, model: nn.Module, dataset: ConceptDataset, n_classes: int, **kwargs):
+        super(CAV, self).__init__(model, dataset)
+        self.n_classes = n_classes
 
-    def fit(self, concept_reps: np.ndarray, concept_labels: np.ndarray) -> None:
-        """
-        Fit the concept classifier to the dataset (latent_reps, concept_labels)
-        Args:
-            kernel: kernel function
-            latent_reps: latent representations of the examples illustrating the concept
-            concept_labels: labels indicating the presence (1) or absence (0) of the concept
-        """
-        super(CAV, self).fit(concept_reps, concept_labels)
-        classifier = SGDClassifier(alpha=0.01, max_iter=1000, tol=1e-3)
-        classifier.fit(concept_reps, concept_labels)
-        self.classifier = classifier
+    def fit(self, device: torch.device, concept_set_size: int = 100) -> None:
+        classifiers = []
+        for concept_id, concept_name in enumerate(self.dataset.concept_names()):
+            classifier = SGDClassifier(alpha=0.01, max_iter=1000, tol=1e-3)
+            X_train, C_train = self.dataset.generate_concept_dataset(concept_id, concept_set_size)
+            H_train = self.model.representation(X_train.to(device)).flatten(start_dim=1)
+            classifier.fit(H_train.detach().cpu().numpy(), C_train.numpy())
+            classifiers.append(classifier)
+        self.classifiers = classifiers
 
-    def predict(self, latent_reps: np.ndarray) -> np.ndarray:
-        """
-        Predicts the presence or absence of the concept for the latent representations
-        Args:
-            latent_reps: representations of the test examples
-        Returns:
-            concepts labels indicating the presence (1) or absence (0) of the concept
-        """
-        return self.classifier.predict(latent_reps)
-
-    def concept_importance(
-        self,
-        latent_reps: np.ndarray,
-        labels: torch.Tensor = None,
-        num_classes: int = None,
-        rep_to_output: callable = None,
-    ) -> np.ndarray:
-        """
-        Predicts the relevance of a concept for the latent representations
-        Args:
-            latent_reps: representations of the test examples
-            labels: the labels associated to the representations one-hot encoded
-            num_classes: the number of classes
-            rep_to_output: black-box mapping the representation space to the output space
-        Returns:
-            concepts scores for each example
-        """
-        one_hot_labels = F.one_hot(labels, num_classes).to(self.device)
-        latent_reps = torch.from_numpy(latent_reps).to(self.device).requires_grad_()
-        outputs = rep_to_output(latent_reps)
-        grads = torch.autograd.grad(outputs, latent_reps, grad_outputs=one_hot_labels)[
-            0
-        ]
-        cav = self.get_activation_vector()
+    def forward(self, x: torch.tensor, y: torch.Tensor) -> torch.Tensor:
+        one_hot_labels = F.one_hot(y, self.n_classes).to(x.device)
+        H = self.model.representation(x).requires_grad_()
+        Y = self.model.representation_to_output(H)
+        grads = torch.autograd.grad(Y, H, grad_outputs=one_hot_labels)[0]
+        cavs = self.get_activation_vectors().to(x.device)
         if len(grads.shape) > 2:
             grads = grads.flatten(start_dim=1)
-        if len(cav.shape) > 2:
-            cav = cav.flatten(start_dim=1)
-        return torch.einsum("bi,bi->b", cav, grads).detach().cpu().numpy()
+        C_sens = torch.einsum("ci,bi->bc", cavs, grads).detach().cpu()
+        return torch.where(C_sens > 0, 1, 0)
 
-    def get_activation_vector(self):
-        return torch.tensor(self.classifier.coef_).to(self.device).float()
+    def get_activation_vectors(self):
+        assert self.classifiers
+        cavs = []
+        for classifier in self.classifiers:
+            cavs.append(torch.tensor(classifier.coef_).float().reshape(1, -1))
+        return torch.cat(cavs, dim=0)

interpretability/example.py
@@ -108,7 +108,7 @@ class InfluenceFunctions(ExampleBasedExplainer):
                 ihvp_prev = ihvp.detach().clone()
                 hvp = self.direct_sum(self.hessian_vector_product(sampled_loss, ihvp_prev))
                 ihvp = grad + (1 - damp) * ihvp - hvp / scale
-            ihvp = ihvp / (scale * len(self.train_loader.dataset))  # Rescale Hessian-Vector products
+            #ihvp = ihvp / (scale * len(self.train_loader.dataset))  # Rescale Hessian-Vector products
             torch.save(ihvp.detach().cpu(), self.save_dir / f"train_ihvp{train_idx}.pt")
         self.ihvp = True
 

models/time_series.py
@@ -106,8 +106,8 @@ class ClassifierECG(ABC, nn.Module):
         test_loader: torch.utils.data.DataLoader,
         save_dir: pathlib.Path,
         lr: int = 1e-03,
-        n_epoch: int = 500,
-        patience: int = 50,
+        n_epoch: int = 200,
+        patience: int = 20,
         checkpoint_interval: int = -1,
         augmentation: bool = True
     ) -> None:
@@ -239,15 +239,15 @@ class StandardCNN(ClassifierECG):
         x = self.cnn2(x)
         x = self.maxpool2(x)
         x = self.cnn3(x)
+        return x
+
+    def representation_to_output(self, x):
         x = self.maxpool3(x)
         x = x.view(x.shape[0], -1)
         x = self.fc1(x)
         x = self.leaky_relu(x)
+        x = self.out(x)
         return x
-
-    def representation_to_output(self, h):
-        h = self.out(h)
-        return h
 
     def last_layer(self) -> nn.Module or None:
         return self.out
@@ -256,25 +256,45 @@ class StandardCNN(ClassifierECG):
 class AllCNN(ClassifierECG):
     def __init__(self, latent_dim: int, name: str = "model"):
         super(AllCNN, self).__init__(latent_dim, name)
-        self.cnn1 = nn.Conv1d(1, latent_dim, kernel_size=3, stride=1, padding=1, padding_mode='circular')
-        self.cnn2 = nn.Conv1d(latent_dim, 2, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        self.cnn1 = nn.Conv1d(1, 16, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        self.cnn2 = nn.Conv1d(16, 64, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        self.cnn3 = nn.Conv1d(64, 128, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        self.cnn4 = nn.Conv1d(128, latent_dim, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        self.out = nn.Conv1d(latent_dim, 2, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        #self.cnn1 = nn.Conv1d(1, latent_dim, kernel_size=3, stride=1, padding=1, padding_mode='circular')
+        #self.cnn2 = nn.Conv1d(latent_dim, 2, kernel_size=3, stride=1, padding=1, padding_mode='circular')
 
     def forward(self, x):
         x = self.cnn1(x)
         x = F.relu(x)
         x = self.cnn2(x)
+        x = F.relu(x)
+        x = self.cnn3(x)
+        x = F.relu(x)
+        x = self.cnn4(x)
+        x = F.relu(x)
+        x = self.out(x)
         x = torch.mean(x, dim=-1)
         return x
 
     def representation(self, x):
-        return F.relu(self.cnn1(x))
+        x = self.cnn1(x)
+        x = F.relu(x)
+        x = self.cnn2(x)
+        x = F.relu(x)
+        x = self.cnn3(x)
+        return x
 
-    def representation_to_output(self, h):
-        h = torch.mean(self.cnn2(h), dim=-1)
-        return h
+    def representation_to_output(self, x):
+        x = F.relu(x)
+        x = self.cnn4(x)
+        x = F.relu(x)
+        x = self.out(x)
+        x = torch.mean(x, dim=-1)
+        return x
 
     def last_layer(self) -> nn.Module or None:
-        return self.cnn2
+        return self.out
 
 
 